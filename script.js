2aee030aa3476a653352a5d28e28157618914f87
Charity Cardinal <charcard@umich.edu>
2018-05-06 21:40:20 -0400

    Made changes

--- script.js
+++ script.js
@@ -1,55 +1,42 @@
 "use strict";
+
+//None of my stuff worked so I deleted most of it and tried to start again like 20 times.  
 class AddressBook {
-    constructor() {
+    constructor(contacts) {
         this.contacts = [];
-    }
+    }   
+    
+      add() {
+        let name = document.getElementById("name").value;
+        let email = document.getElementById("email").value;
+        let phone = document.getElementById("phone").value;
+        let relation = document.getElementById("relation").value;
+        let newContact = new Contacts(name.add, email.add, phone.add, relation.add);
 
-    add(info) {
-        let newPerson = new Contacts(info.name, info.email, info.phone, info.occupation);
-        this.contacts.push(newPerson);
-        console.log(this.contacts);
-    }
-    deleteAt(index) {
+        NewAddressBook.contacts.push(Contacts);
+
+    
+      display(){
+    
+      }
+    
+    
+      deleteAt(index) {
         this.contacts.splice(index, 1);
+      }
     }
-    print() {
-        for (let i = 0; i < this.contacts.length; i++) {
-            console.log(this.contacts[i]);
-        }
+    
+        class Contacts {
+        constructor(name, email, phone, relation) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.relation = relation;
+      }
     }
-}
-
-const NewAddressBook = new AddressBook();
-
-class Contacts {
-    constructor(name, email, phone, relation) {
-    this.name = name;
-    this.email = email;
-    this.phone = phone;
-    this.relation = relation;
-    }
-}
-console.log(NewAddressBook);
-
-while (true) {
-    let userInput = prompt("Would you like to add, delete, print, or quit?");
-if (userInput.toLowerCase() === "add") {
-    let info = {
-        name: prompt("Enter your full name."),
-        phone: prompt("Enter you phone number."),
-        email: prompt("Enter your email."),
-        relation: prompt("Enter your relation."),
-    };
-        NewAddressBook.add(info);
-
-    }else if (userInput.toLowerCase() === "delete") {
-        let index = prompt("Which index would you like to remove?");
-        NewAddressBook.deleteAt(index);
-
-        }else if (userInput.toLowerCase() === "print") {
-            NewAddressBook.print();
-
-            }else if (userInput.toLowerCase() === "quit") {
-            break;
-            }
-}
+    
+    const NewAddressBook = new AddressBook();
+    
+    document.getElementById("add").addEventListener("click", NewAddressBook.add); 
+    
+    NewAddressBook.display();
